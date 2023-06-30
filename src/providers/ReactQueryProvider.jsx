77ad985ef8e8c2989a7a28a7cb4e40f8bcee8185
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-06-30 11:38:24 +0300

    feat: set network mode to 'always'

--- src/providers/ReactQueryProvider.jsx
+++ src/providers/ReactQueryProvider.jsx
@@ -2,7 +2,17 @@ import React, { Fragment } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      networkMode: "always",
+    },
+    mutations: {
+      networkMode: "always",
+    },
+  },
+});
 
 export const ReactQueryProvider = (props) => {
   return (
