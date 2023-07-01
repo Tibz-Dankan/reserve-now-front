32515130a3d12a846d8b64dba74cdeb0156ad3aa
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-01 09:57:26 +0300

    refactor: change Card component UI styles to tailwind

--- src/modules/booking/Pages/Booking.jsx
+++ src/modules/booking/Pages/Booking.jsx
@@ -1,9 +1,15 @@
 import React, { Fragment } from "react";
+import { Modal } from "../../../shared/UI/Modal";
 
 export const Booking = () => {
   return (
     <Fragment>
-      <div>Booking</div>
+      <div>
+        <span>Booking</span>
+        <Modal openModalElement={<button>Open booking modal</button>}>
+          This is the booking modal content
+        </Modal>
+      </div>
     </Fragment>
   );
 };
