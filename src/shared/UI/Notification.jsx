d665e9ac761b6e131e4fbae1857d799d05304203
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-01 15:03:14 +0300

    refactor: switch to svg icons

--- src/shared/UI/Notification.jsx
+++ src/shared/UI/Notification.jsx
@@ -1,56 +1,43 @@
 import React from "react";
 import { Card } from "./Card";
-import { BsCheckCircleFill } from "react-icons/bs";
-import { IconContext } from "react-icons";
-import { MdCancel, MdInfo, MdWarning } from "react-icons/md";
-import { FaWindowClose } from "react-icons/fa";
+import sprite from "../../assets/icons/sprite.svg";
 
 export const Notification = (props) => {
   const type = props.type;
   let bgColor;
+  let icon;
 
-  const icon = () => {
-    if (type === "success") {
-      bgColor = "bg-green-500";
-      return <BsCheckCircleFill />;
-    } else if (type === "error") {
-      bgColor = "bg-red-500";
-      return <MdCancel />;
-    } else if (type === "info") {
-      bgColor = "bg-blue-500";
-      return <MdInfo />;
-    } else if (type === "warning") {
-      bgColor = "bg-yellow-400";
-      return <MdWarning />;
-    } else {
-      bgColor = "bg-blue-500";
-      return <MdInfo />;
-    }
-  };
-  icon();
+  if (type === "success") {
+    icon = "check-circle";
+    bgColor = "bg-green-500";
+  } else if (type === "error") {
+    icon = "cancel-circle";
+    bgColor = "bg-red-500";
+  } else if (type === "info") {
+    icon = "info";
+    bgColor = "bg-blue-500";
+  } else if (type === "warning") {
+    icon = "warning";
+    bgColor = "bg-yellow-400";
+  } else {
+    icon = "info";
+    bgColor = "bg-blue-500";
+  }
 
   return (
     <Card
       className={`${bgColor} text-lg z-50 fixed top-5 right-5 flex items-center rounded w-72`}
     >
-      <span
+      <svg
         className="w-3 h-3 fill-white absolute right-4 top-1"
         onClick={props.onClose}
       >
-        <IconContext.Provider
-          value={{ size: "20px", color: "hsl(0, 0%, 100%)" }}
-        >
-          <FaWindowClose />
-        </IconContext.Provider>
-      </span>
+        <use href={`${sprite}#icon-cross`}></use>
+      </svg>
       <div className="pt-3">
-        <span className="grid place-items-center ">
-          <IconContext.Provider
-            value={{ size: "20px", color: "hsl(0, 0%, 100%)" }}
-          >
-            {icon()}
-          </IconContext.Provider>
-        </span>
+        <svg className="grid place-items-center ">
+          <use href={`${sprite}#icon-${icon}`}></use>
+        </svg>
       </div>
       <div className="flex items-center p-1 mt-1 ml-2 max-w-[230] text-white">
         <span className="text-sm"> {props.message}</span>
