ab1d917502bcc7ca31f11e1760eb797bdfa141da
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-02 03:38:04 +0300

    feat: add icon button

---
+++ src/shared/UI/IconButton.jsx
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import sprite from "../../assets/icons/sprite.svg";
+
+export const IconButton = (props) => {
+  return (
+    <Fragment>
+      <button
+        className={`${props.className} flex justify-center items-center w-full h-9 bg-primary text-gray-light-1 rounded uppercase font-bold`}
+        onClick={props.onClick}
+        type={props.type}
+        disabled={props.disabled}
+      >
+        <svg className={`${props.iconClass}`}>
+          <use href={`${sprite}#icon-${props.icon}`}></use>
+        </svg>
+        <span>{props.label}</span>
+      </button>
+    </Fragment>
+  );
+};
