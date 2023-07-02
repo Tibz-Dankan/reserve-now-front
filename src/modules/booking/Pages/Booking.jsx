777ad726c9f75c188218cd544da5ed246909ce68
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-02 15:48:16 +0300

    refactor: add all icons to sprite

--- src/modules/booking/Pages/Booking.jsx
+++ src/modules/booking/Pages/Booking.jsx
@@ -1,15 +1,18 @@
 import React, { Fragment } from "react";
 import { Modal } from "../../../shared/UI/Modal";
+import { MasterLayout } from "../../../shared/layouts/MasterLayout";
 
 export const Booking = () => {
   return (
     <Fragment>
-      <div>
-        <span>Booking</span>
-        <Modal openModalElement={<button>Open booking modal</button>}>
-          This is the booking modal content
-        </Modal>
-      </div>
+      <MasterLayout title="Booking">
+        <div>
+          <span>Booking</span>
+          <Modal openModalElement={<button>Open booking modal</button>}>
+            This is the booking modal content
+          </Modal>
+        </div>
+      </MasterLayout>
     </Fragment>
   );
 };
