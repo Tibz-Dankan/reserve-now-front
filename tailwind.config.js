c202910bd996ada72a01057edd7b637ee862132f
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-20 22:03:37 +0300

    refactor: change to tailwind standard view port width config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ export default {
   darkMode: "class",
   theme: {
     screens: {
-      sm: "480px",
+      sm: "640px",
       md: "768px",
-      lg: "976px",
-      xl: "1440px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {
       colors: {
