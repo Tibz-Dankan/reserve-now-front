8aa0549468ede504b81937715d50d432aade52c1
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-23 19:07:26 +0300

    feat: hover effect on the scroll to top button

--- src/shared/UI/ScrollToTop.jsx
+++ src/shared/UI/ScrollToTop.jsx
@@ -14,7 +14,8 @@ export const ScrollToTop = () => {
       <div className="relative">
         <div
           className="w-10 h-10 flex items-center justify-center bg-primary-dark
-            rounded-[50%] border-[1px] border-primary absolute left-4 bottom-4"
+            rounded-[50%] border-[1px] border-primary absolute left-4 bottom-4
+            hover:bg-primary transition-transform cursor-pointer"
           onClick={scrollToTop}
         >
           <svg className="w-[24px] h-[24px] text-gray-light-3">
