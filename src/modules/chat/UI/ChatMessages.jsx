1456c4f9840e58a417ce5dac05a39cd08c0cfcc9
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-07-25 20:01:16 +0300

    feat(UI): add card for primary chat message

--- src/modules/chat/UI/ChatMessages.jsx
+++ src/modules/chat/UI/ChatMessages.jsx
@@ -1,9 +1,58 @@
 import { Fragment } from "react";
+import { MessagePrimary } from "./MessagePrimary";
 
+// export const ChatMessages = ({ messageList }) => {
 export const ChatMessages = () => {
+  const messageList = [
+    {
+      senderId: "user1",
+      recipientId: "user2",
+      createdAt: "2023-07-25T12:00:00Z",
+      senderImageUrl: "https://example.com/user1.jpg",
+      recipientImageUrl: "https://example.com/user2.jpg",
+      isRead: true,
+      isDelivered: true,
+      message: "Hello, how are you?",
+    },
+    {
+      senderId: "user2",
+      recipientId: "user1",
+      createdAt: "2023-07-25T13:15:00Z",
+      senderImageUrl: "https://example.com/user2.jpg",
+      recipientImageUrl: "https://example.com/user1.jpg",
+      isRead: false,
+      isDelivered: true,
+      message: "I'm doing great! Thanks for asking.",
+    },
+    {
+      senderId: "user1",
+      recipientId: "user3",
+      createdAt: "2023-07-25T14:30:00Z",
+      senderImageUrl: "https://example.com/user1.jpg",
+      recipientImageUrl: "https://example.com/user3.jpg",
+      isRead: true,
+      isDelivered: true,
+      message: "Hey there, what's up?",
+    },
+    {
+      senderId: "user3",
+      recipientId: "user1",
+      createdAt: "2023-07-25T15:45:00Z",
+      senderImageUrl: "https://example.com/user3.jpg",
+      recipientImageUrl: "https://example.com/user1.jpg",
+      isRead: true,
+      isDelivered: false,
+      message: "Not much, just working on some stuff.",
+    },
+  ];
+
+  // TODO: to sort messages accordingly
+
   return (
     <Fragment>
-      <div>chatMessages</div>
+      <div className="p-4 pt-8">
+        <MessagePrimary />
+      </div>
     </Fragment>
   );
 };
---
+++ src/modules/chat/UI/MessagePrimary.jsx
@@ -0,0 +1,49 @@
+import { Fragment } from "react";
+import sprite from "../../../assets/icons/sprite.svg";
+
+// export const MessagePrimary = ({ message }) => {
+export const MessagePrimary = () => {
+  const showImage = false;
+
+  return (
+    <Fragment>
+      <div>
+        <div className="bg-green- flex items-center gap-x-1">
+          <div
+            className="bg-gray-light-3 flex items-center justify-center 
+                w-10 h-10 rounded-[50%] relative"
+          >
+            {showImage && (
+              <img
+                src={"recipient.imageUrl"}
+                alt={"recipient.username"}
+                className="w-full  h-full rounded-[50%]"
+              />
+            )}
+            {!showImage && (
+              <svg className="w-[24px] h-[24px] fill-gray-dark-1">
+                <use href={`${sprite}#icon-person-filled`}></use>
+              </svg>
+            )}
+          </div>
+          <span>Tibesigwa Dankan</span>
+          <svg className="w-[12px] h-[12px] fill-gray-500">
+            <use href={`${sprite}#icon-dot`}></use>
+          </svg>
+          <span className="text-sm text-gray-500">6:36 PM</span>
+        </div>
+        <div className="mt-3 ml-9">
+          <p
+            className="text-sm text-gray-900 bg-gray-light-3 p-4 rounded-xl 
+            rounded-tl-none relative before:absolute before:top-[-8px] before:left-0 
+            before:h-4 before:w-8 before:bg-gray-light-3 before:skew-y-[32deg] 
+            before:rotate-[-8deg] w-auto max-w-[300px] min-h-[32px]"
+          >
+            Chat message here Chat message here Chat message here Chat message
+            here Chat message here
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
