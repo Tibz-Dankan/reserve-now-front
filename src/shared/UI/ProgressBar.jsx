18b589d86438be055558c29deae2f2fd5f28ab15
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-08-11 19:20:25 +0300

    feat: dynamically show filled color underway

--- src/shared/UI/ProgressBar.jsx
+++ src/shared/UI/ProgressBar.jsx
@@ -12,6 +12,22 @@ export const ProgressBar = (props) => {
   const stages = generateStagesArray();
   const stageLabelList = props.stageLabelList;
 
+  const filledCircle = (index) => {
+    const currentStage = 2;
+    console.log("currentStage === index");
+    console.log(currentStage === index);
+    return currentStage === index;
+  };
+
+  // const filledCircle = (index) => props.currentStage === index;
+  const filledBar = (index) => {
+    const currentStage = 2;
+    console.log("currentStage < index");
+    console.log(currentStage < index);
+    return currentStage < index;
+  };
+  // const filledBar = (index) => props.currentStage < index;
+
   return (
     <Fragment>
       <div className="w-full flex items-center justify-center mb-4 mt-12">
@@ -28,8 +44,12 @@ export const ProgressBar = (props) => {
                       stageLabelList[index].label}
                   </label>
                   <span
-                    className="w-10 h-10 bg-primary flex items-center justify-center 
-                     rounded-[50%] text-gray-light-1"
+                    className={`w-10 h-10 bg-primary flex items-center justify-center 
+                     rounded-[50%] text-gray-light-1 ${
+                       filledCircle(index)
+                         ? "bg-primary text-gray-light-1"
+                         : "bg-gray-light-3 text-gray-900"
+                     }`}
                   >
                     {stage}
                   </span>
@@ -39,18 +59,26 @@ export const ProgressBar = (props) => {
                 <div className="flex items-center justify-start relative">
                   <label
                     className="absolute top-[-32px] left-[-32px] w-auto h-auto
-                          text-gray-dark-2 text-center font-bold"
+                    text-gray-dark-2 text-center font-bold"
                   >
                     {stage === stageLabelList[index].stage &&
                       stageLabelList[index].label}
                   </label>
                   <span
-                    className="w-10 h-10 bg-primary flex items-center justify-center 
-                    rounded-[50%] text-gray-light-1"
+                    className={`w-10 h-10 bg-primary flex items-center justify-center 
+                     rounded-[50%] text-gray-light-1 ${
+                       filledCircle(index)
+                         ? "bg-primary text-gray-light-1"
+                         : "bg-gray-light-3 text-gray-900"
+                     }`}
                   >
                     {stage}
                   </span>
-                  <span className="h-3 w-[160px] mx-[-2px] bg-primary"></span>
+                  <span
+                    className={`h-3 w-[160px] mx-[-2px] ${
+                      filledBar(index) ? "bg-primary" : "bg-gray-light-3"
+                    }`}
+                  ></span>
                 </div>
               )}
             </div>
