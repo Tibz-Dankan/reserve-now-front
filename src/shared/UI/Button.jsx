51742c2349d4819769c16a2912e9d3312ae15622
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-08-25 19:09:36 +0300

    fix: override tailwind classes in the button and loader components

--- src/shared/UI/Button.jsx
+++ src/shared/UI/Button.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const Button = (props) => {
   return (
     <Fragment>
       <button
-        className={`${props.className} h-9 bg-primary text-gray-light-1 rounded px-3`}
+        className={twMerge(
+          "h-9 bg-primary text-gray-light-1 rounded px-3",
+          props.className
+        )}
         type={props.type}
         onClick={props.onClick}
         disabled={props.disabled ? props.disabled : false}
--- src/shared/UI/Loader.jsx
+++ src/shared/UI/Loader.jsx
@@ -1,14 +1,18 @@
 import React, { Fragment } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const Loader = (props) => {
   return (
     <Fragment>
       <div
-        className={`${props.className} h-10 bg-primary rounded-md relative 
-         before:border-4 before:border-solid before:border-gray-light-4 
-         before:border-r-4 before:border-r-gray-light-1 before:w-[24px] 
-         before:h-[24px] before:rounded-[50%] before:absolute before:top-2
-         before:left-4 before:animate-rotate`}
+        className={twMerge(
+          `h-10 bg-primary rounded-md relative 
+          before:border-4 before:border-solid before:border-gray-light-4 
+          before:border-r-4 before:border-r-gray-light-1 before:w-[24px] 
+          before:h-[24px] before:rounded-[50%] before:absolute before:top-2
+          before:left-4 before:animate-rotate`,
+          props.className
+        )}
       >
         <span className="text-gray-light-1 absolute top-2 left-12 font-bold">
           {props?.label && props.label + "..."}
